d6be24b9d5bfd4cf101abcf903826a9956130761
Micah Gordon <micahgordon66@gmail.com>
2018-06-24 23:11:33 +0300

    add routes for getting products and getting categories

--- store.py
+++ store.py
@@ -3,10 +3,16 @@ from sys import argv
 import json
 import pymysql
 
+connection = pymysql.connect(host='localhost',
+						  user='root',
+						  password='root',
+						  db='store_adv',
+						  charset='utf8',
+						  cursorclass=pymysql.cursors.DictCursor)
+
 @get("/admin")
 def admin_portal():
 	return template("pages/admin.html")
-
 
 
 @get("/")
@@ -28,5 +34,87 @@ def stylesheets(filename):
 def images(filename):
     return static_file(filename, root='images')
 
+
+@post('/category')
+def create_category():
+
+	try:
+		with connection.cursor() as cursor:
+			sql = "INSERT INTO CATEGORIES(name) VALUES();"
+			cursor.execute(sql)
+			connection.commit()
+			id = cursor.lastrowid
+			return json.dumps({
+				'STATUS': 'SUCCESS',
+				'CAT_ID': id,
+				'CODE': 201
+			})
+	except Exception as e:
+		return json.dumps({
+			'STATUS': 'ERROR',
+			'MSG': repr(e),
+			'CODE': 500
+		})
+
+
+
+@get('/categories')
+def get_categories():
+	try:
+		with connection.cursor() as cursor:
+			sql = "SELECT * FROM CATEGORIES;"
+			cursor.execute(sql)
+			result = cursor.fetchall()
+		return json.dumps({
+			'STATUS': 'SUCCESS',
+			'CATEGORIES': result,
+			'CODE': {
+				200: 'Success',
+			}
+		})
+	except Exception as e:
+		return json.dumps({
+			'STATUS': 'ERROR',
+			'MSG': repr(e),
+			'CODE': 500
+		})
+
+# @get('/category/<id>/products')
+# def get_products_by_id(id):
+# 	return ({
+# 	'STATUS': {
+# 		'SUCCESS': 'categories fetched',
+# 		'ERROR': 'internal error'
+# 		},
+# 		'MSG': 'internal error',
+# 		'PRODUCTS': [],
+# 		'CODE': {
+# 			200: 'Success',
+# 			404: 'category not found',
+# 			500: 'internal error'
+# 		}
+# 	})
+
+
+@get('/products')
+def get_products():
+	try:
+		with connection.cursor() as cursor:
+			sql = "SELECT * FROM PRODUCTS;"
+			cursor.execute(sql)
+			result = cursor.fetchall()
+		return json.dumps({
+			'STATUS': 'SUCCESS',
+			'CATEGORIES': result,
+			'CODE': {
+				200: 'Success',
+			}
+		})
+	except Exception as e:
+		return json.dumps({
+			'STATUS': 'ERROR',
+			'MSG': repr(e),
+			'CODE': 500
+		})
 
 run(host='0.0.0.0', port=argv[1])
